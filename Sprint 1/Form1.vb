2dadbba764885f3eca65e0bf71f3502f4a180f83
NelsRykk <Nelsrykk@isu.edu>
2019-12-05 17:45:48 -0800

    sprint 3

--- Sprint 1/Form1.vb
+++ Sprint 1/Form1.vb
@@ -3,27 +3,26 @@
 'Sprint 1 
 '12/3/19
 'https://github.com/NelsRykk/RJN-VS-F19/tree/master/Sprint%201
-
 Option Explicit On
 Option Strict On
 Public Class Form1
 
     'Sprint 1
-    Private Sub ConvertButton_Click(typedString As String, convertString As Int32) Handles ConvertButton.Click
+    'Private Sub ConvertButton_Click(typedString As String, convertString As Int32) Handles ConvertButton.Click
+    '    Try
+    '        typedString = StringIntegerTextBox.Text
+    '        convertString = CType(StringIntegerTextBox.Text, Int32)
+    '        ConvertedTextBox.Text = CStr(convertString)
+    '        StringIntegerTextBox.Text = ""
+    '    Catch ex As Exception
+    '        MessageBox.Show("This must be an Integer Value")
+    '    End Try
+    '    ValidateAndConvert(typedString, convertString)
+    'End Sub
 
-        Try
-            typedString = StringIntegerTextBox.Text
-            convertString = CType(StringIntegerTextBox.Text, Int32)
-            ConvertedTextBox.Text = CStr(convertString)
-            StringIntegerTextBox.Text = ""
-        Catch ex As Exception
-            MessageBox.Show("This must be an Integer Value")
-        End Try
-        ValidateAndConvert(typedString, convertString)
-    End Sub
     Private Sub Collect(ByVal integerData As Integer)
         'TestValidateAndCovert(integerData)
-        GetUserInput(integerData)
+        ' GetUserInput(integerData)
 
     End Sub
     'Sprint2
@@ -160,26 +159,34 @@ Public Class Form1
     'In as few lines as possible produce the same results as the function IsWayTooLong()
     'Hint use a loop and a List or Array
     'Also only have one return statement at the end
+    'Private Function ShortAndSweet(ByVal numberFromZeroToFifteen As Integer) As String
+    '    'Your code here
+    '    If numberFromZeroToFifteen = 0 Then : Return "Your Number is : Zero"
+    '    ElseIf numberFromZeroToFifteen = 0 Then : Return "Your Number is : Zero"
+    '    ElseIf numberFromZeroToFifteen = 1 Then : Return "Your Number is : One"
+    '    ElseIf numberFromZeroToFifteen = 2 Then : Return "Your Number is : Two"
+    '    ElseIf numberFromZeroToFifteen = 3 Then : Return "Your Number is : Three"
+    '    ElseIf numberFromZeroToFifteen = 4 Then : Return "Your Number is : Four"
+    '    ElseIf numberFromZeroToFifteen = 5 Then : Return "Your Number is : Five"
+    '    ElseIf numberFromZeroToFifteen = 6 Then : Return "Your Number is : Six"
+    '    ElseIf numberFromZeroToFifteen = 7 Then : Return "Your Number is : Seven"
+    '    ElseIf numberFromZeroToFifteen = 8 Then : Return "Your Number is : Eight"
+    '    ElseIf numberFromZeroToFifteen = 9 Then : Return "Your Number is : Nine"
+    '    ElseIf numberFromZeroToFifteen = 10 Then : Return "Your Number is : Ten"
+    '    ElseIf numberFromZeroToFifteen = 11 Then : Return "Your Number is : Eleven"
+    '    ElseIf numberFromZeroToFifteen = 12 Then : Return "Your Number is : Twelve"
+    '    ElseIf numberFromZeroToFifteen = 13 Then : Return "Your Number is : Thirteen"
+    '    ElseIf numberFromZeroToFifteen = 14 Then : Return "Your Number is : Fourteen"
+    '    ElseIf numberFromZeroToFifteen = 15 Then : Return "Your Number is : Fifteen"
+    '    ElseIf numberFromZeroToFifteen < 0 Or numberFromZeroToFifteen > 15 Then : Return "your number is outside of specified parameters"
+    '    End If
+    'End Function
     Private Function ShortAndSweet(ByVal numberFromZeroToFifteen As Integer) As String
-        'Your code here
-        If numberFromZeroToFifteen = 0 Then : Return "Your Number is : Zero"
-        ElseIf numberFromZeroToFifteen = 0 Then : Return "Your Number is : Zero"
-        ElseIf numberFromZeroToFifteen = 1 Then : Return "Your Number is : One"
-        ElseIf numberFromZeroToFifteen = 2 Then : Return "Your Number is : Two"
-        ElseIf numberFromZeroToFifteen = 3 Then : Return "Your Number is : Three"
-        ElseIf numberFromZeroToFifteen = 4 Then : Return "Your Number is : Four"
-        ElseIf numberFromZeroToFifteen = 5 Then : Return "Your Number is : Five"
-        ElseIf numberFromZeroToFifteen = 6 Then : Return "Your Number is : Six"
-        ElseIf numberFromZeroToFifteen = 7 Then : Return "Your Number is : Seven"
-        ElseIf numberFromZeroToFifteen = 8 Then : Return "Your Number is : Eight"
-        ElseIf numberFromZeroToFifteen = 9 Then : Return "Your Number is : Nine"
-        ElseIf numberFromZeroToFifteen = 10 Then : Return "Your Number is : Ten"
-        ElseIf numberFromZeroToFifteen = 11 Then : Return "Your Number is : Eleven"
-        ElseIf numberFromZeroToFifteen = 12 Then : Return "Your Number is : Twelve"
-        ElseIf numberFromZeroToFifteen = 13 Then : Return "Your Number is : Thirteen"
-        ElseIf numberFromZeroToFifteen = 14 Then : Return "Your Number is : Fourteen"
-        ElseIf numberFromZeroToFifteen = 15 Then : Return "Your Number is : Fifteen"
-        ElseIf numberFromZeroToFifteen < 0 Or numberFromZeroToFifteen > 15 Then : Return "your number is outside of specified parameters"
+        Dim numberArray(16, 0) As String
+        Dim word As String
+        numberArray = {{"Zero"}, {"One"}, {"Two"}, {"Three"}, {"Four"}, {"Five"}, {"Six"}, {"Seven"}, {"Eight"}, {"Nine"}, {"Ten"}, {"Eleven"}, {"Twelve"}, {"Thirteen"}, {"Fourteen"}, {"Fifteen"}}
+        If numberFromZeroToFifteen > 15 Or numberFromZeroToFifteen < 0 Then : Return "your number is outside of specified parameters"
+        Else word = CStr(numberArray(numberFromZeroToFifteen, 0)) : Return word
         End If
     End Function
 
